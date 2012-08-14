7d8116ac981f6186c60d7643a67465f5889e9d60
James Ward <james@jamesward.org>
2012-08-14 07:44:19 -0600

    change name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hello-clojure-compojure "0.1.0-SNAPSHOT"
+(defproject hello-clojure-noir "0.1.0-SNAPSHOT"
   :main web
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.2.1"]])
